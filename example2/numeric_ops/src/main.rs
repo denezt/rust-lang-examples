5580ab23cf48769cb166bc16653608aee3cd14c1
URUS CG <uruscg@gmail.com>
2019-12-10 17:58:18 +0000

    Adding directory

---
+++ example2/numeric_ops/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+	// addition
+    	let sum = 5 + 10;
+	println!("The value of sum is: {}", sum);
+    	// subtraction
+    	let difference = 95.5 - 4.3;
+	println!("The value of difference is: {}", difference);
+
+    	// multiplication
+    	let product = 4 * 30;
+	println!("The value of product is: {}", product);
+    	// division
+    	let quotient = 56.7 / 32.2;
+	println!("The value of quotient is: {}", quotient);
+    	// remainder
+    	let remainder = 43 % 5;
+	println!("The value of remainder is: {}", remainder);
+
+}
---
+++ example2/numeric_ops/target/debug/numeric_ops.d
@@ -0,0 +1,1 @@
+/programming/rust/rust-lang-examples/example2/numeric_ops/target/debug/numeric_ops: /programming/rust/rust-lang-examples/example2/numeric_ops/src/main.rs
